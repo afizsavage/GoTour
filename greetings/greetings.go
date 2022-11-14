259f358cad7d62a5b3c332c86ceee42c329a29fb
Afiz Savage <savageafiz9@gmail.com>
2022-11-14 15:38:44 +0000

    get started with multi module workspace

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -11,7 +11,7 @@ func Hello(name string) (string, error) {
     if name == "" {
         return name, errors.New("empty name")
     }
-    message := fmt.Sprintf(randomFormat())
+    message := fmt.Sprintf(randomFormat(), name)
     return message, nil
 }
 

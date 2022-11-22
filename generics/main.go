caa5fadea14eaff5f5ab4ce938cf12f31979d3ed
Afiz Savage <savageafiz9@gmail.com>
2022-11-22 19:26:38 +0000

    declare a type constraint

--- generics/main.go
+++ generics/main.go
@@ -3,6 +3,10 @@ package main
 
 import "fmt"
 
+type Number interface {
+    int64 | float64
+}
+
 func main() {
     // Initialize a map for the integer values
     ints := map[string]int64{
@@ -21,8 +25,12 @@ func main() {
         SumFloats(floats))
 
 	fmt.Printf("Generic Sums: %v and %v\n",
-		SumIntsOrFloats[string, int64](ints),
-		SumIntsOrFloats[string, float64](floats))
+		SumIntsOrFloats(ints),
+		SumIntsOrFloats(floats))
+
+	fmt.Printf("Generic Sums with Constraint: %v and %v\n",
+		SumNumbers(ints),
+		SumNumbers(floats))
 }
 
 func SumInts(m map[string]int64) int64 {
@@ -52,3 +60,12 @@ func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
     return s
 }
 
+// SumNumbers sums the values of map m. It supports both integers
+// and floats as map values.
+func SumNumbers[K comparable, V Number](m map[K]V) V {
+    var s V
+    for _, v := range m {
+        s += v
+    }
+    return s
+}
